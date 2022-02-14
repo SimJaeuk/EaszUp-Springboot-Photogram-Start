e24cd1a0a2e44dd640da704b7c597ad75523e9ac
SimJaeuk <jaehook293@gmail.com>
2022-02-14 20:19:01 +0900

    회원가입

--- src/main/java/com/cos/photogramstart/domain/user/User.java
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -2,11 +2,13 @@ package com.cos.photogramstart.domain.user;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
+import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,12 +26,15 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략이 데이터베이스를 따라간다.
 	private int id;
 	
+	@Column(length = 20, unique = true)
 	private String username;
+	@Column(nullable = false)
 	private String password;
-	
+	@Column(nullable = false)
 	private String name;
 	private String website; // 웹 사이트
 	private String bio; // 자기소개
+	@Column(nullable = false)
 	private String email;
 	private String phone;
 	private String gender;
