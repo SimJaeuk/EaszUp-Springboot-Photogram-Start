acef1174e32e6355e2004291172e63fff29b6aba
SimJaeuk <jaehook293@gmail.com>
2022-02-21 18:45:38 +0900

    회원정보수정

--- src/main/java/com/cos/photogramstart/service/UserService.java
+++ src/main/java/com/cos/photogramstart/service/UserService.java
@@ -1,11 +1,13 @@
 package com.cos.photogramstart.service;
 
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.domain.user.UserRepository;
+import com.cos.photogramstart.handler.ex.CustomValidationApiException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,7 +21,9 @@ public class UserService {
 	@Transactional
 	public User 회원수정(int id, User user) {
 		// 1. 영속화
-		User userEntity = userRepository.findById(id).get(); // 1. 무조건 찾았다. get() 2. 못찾았어 익셉션 발동시킬게 orElseThorw()
+		// 1. 무조건 찾았다. get() 2. 못찾았어 익셉션 발동시킬게 orElseThorw()
+		User userEntity = userRepository.findById(id).orElseThrow(() -> {return new CustomValidationApiException("찾을 수 없는 ID입니다.");});
+		
 
 		// 2. 영속화된 오브젝트 수정 -> 더티체킹 (업데이트 완료)
 		userEntity.setName(user.getName());
--- src/main/resources/static/js/update.js
+++ src/main/resources/static/js/update.js
@@ -1,6 +1,7 @@
 // (1) 회원정보 수정
-function update(userId) {
+function update(userId, event) {
 
+	event.preventDefault(); // 폼태그 액션을 막기
 	let data = $("#profileUpdate").serialize();
 
 	console.log(data);
@@ -11,10 +12,14 @@ function update(userId) {
 		data : data,
 		contentType: "application/x-www-form-urlencoded; charset=utf-8",
 		dataType: "json"
-		}).done(res=>{
-			console.log("update 성공");
+		}).done(res=>{ // HttpStatue 상태코드 200번대
+			console.log("성공", res); 
 			location.href = `/user/${userId}`;
-		}).fail(error=>{
-			console.log("update 실패");
+		}).fail(error=>{ // HttpStatue 상태코드 200번대가 아닐 때 fail
+			if(error.data == null){
+				alert(error.responseJSON.message);
+			}else {
+			alert(JSON.stringify(error.responseJSON.data));
+			}
 		});
 }
