2b765d4a84542b8bc1c218d7892e7b0c4a720bd1
SimJaeuk <jaehook293@gmail.com>
2022-03-08 22:27:43 +0900

    프로필 페이지

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.cos.photogramstart.handler;
 
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomApiException;
+import com.cos.photogramstart.handler.ex.CustomException;
 import com.cos.photogramstart.handler.ex.CustomValidationApiException;
 import com.cos.photogramstart.handler.ex.CustomValidationException;
 import com.cos.photogramstart.util.Script;
@@ -23,21 +23,26 @@ public class ControllerExceptionHandler {
 		// 1. 클라이언트에게 응답할 때는 Script가 좋음.
 		// 2. Ajax통신 - CMRespDto
 		// 3. Android 통신 - CmRespDto
-		if(e.getErrorMap() == null) {
+		if (e.getErrorMap() == null) {
 			return Script.back(e.getMessage());
-		}else {
-			return Script.back(e.getErrorMap().toString());	
+		} else {
+			return Script.back(e.getErrorMap().toString());
 		}
-		
-	}	
-	
+
+	}
+
+	@ExceptionHandler(CustomException.class)
+	public String exception(CustomException e) {
+		return Script.back(e.getMessage());
+	}
+
 	@ExceptionHandler(CustomValidationApiException.class)
 	public ResponseEntity<?> validationApiException(CustomValidationApiException e) {
 		return new ResponseEntity<>(new CMRespDto<>(-1, e.getMessage(), e.getErrorMap()), HttpStatus.BAD_REQUEST);
-	}	
-	
+	}
+
 	@ExceptionHandler(CustomApiException.class)
 	public ResponseEntity<?> apiException(CustomValidationApiException e) {
 		return new ResponseEntity<>(new CMRespDto<>(-1, e.getMessage(), null), HttpStatus.BAD_REQUEST);
-	}	
+	}
 }
